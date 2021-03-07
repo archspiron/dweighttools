7776609d578b1305a577c3e955c3e461ac317526
archspiron <78673425+archspiron@users.noreply.github.com>
2021-03-06 20:43:52 -0600

    Add files via upload
    v2(not-working)

--- calc.py
+++ calc.py
@@ -1,18 +1,24 @@
 import math
 
+gate = int(input('SELECT 1 FOR MANUAL DIMS INPUT, SELECT 2 FOR TYPE SELECTION: '))
+
 while True:
- if input('1 FOR MANUAL INPUT, 2 FOR BOXTYPE') == '1':
-    x = int(input("Enter X:"))
-    y = int(input("Enter Y:"))
-    z = int(input("Enter Z:"))
 
-    result = x * y * z
-    result = math.ceil(result/139.0)
-    print(result)
+ if gate == '1':
 
- if input ('1 FOR MANUAL INPUT, 2 FOR BOXTYPE') == '2':
-    print('TG D-WEIGHT IS 19LBS')
-    print('UG D-WEIGHT IS 31LBS')
-    print('PG D-WEIGHT IS TBD')
- if input('Do you want to repeat(y/n)') == 'n':
-        break
+	 x = int(input('ENTER X : '))
+	 y = int(input('ENTER Y : '))
+	 z = int(input('ENTER Z : '))
+
+	 result = x * y * z
+	 result = math.ceil(result/139.0)
+	 print(result)
+	
+
+
+
+#if gate == '2':
+ #    print('TG D-WEIGHT IS 19LBS')
+  #   print('UG D-WEIGHT IS 31LBS')
+   #  print('PG D-WEIGHT IS TBD')
+ 
