4d44f162437c7fde4d30aad8beb85bfed80ff0ba
archspiron <78673425+archspiron@users.noreply.github.com>
2021-03-06 22:02:16 -0600

    Add files via upload

--- calc.py
+++ calc.py
@@ -1,9 +1,7 @@
 import math
 
-gate = int(input('SELECT 1 FOR MANUAL DIMS INPUT, SELECT 2 FOR TYPE SELECTION: '))
-
 while True:
-
+ gate = int(input('SELECT 1 FOR MANUAL DIMS INPUT, SELECT 2 FOR TYPE SELECTION: '))
  if gate == '1':
 
 	 x = int(input('ENTER X : '))
@@ -13,18 +11,21 @@ while True:
 	 result = x * y * z
 	 result = math.ceil(result/139.0)
 	 print(result)
- if input('REPEAT?(Y/N): ') == 'n':
-	 break
-
-
-while True:
 
  if gate == '2':
  
-     print('TG D-WEIGHT IS 19LBS')
-     print('UG D-WEIGHT IS 31LBS')
-     print('PG D-WEIGHT IS TBD')
+     option = str(input('INPUT TYPE : '))
+	 
+     if option == 'tg' or 'TG':
+	     print('TG D-WEIGHT IS 19LBS')
+	
+     if option == 'ug' or 'UG':
+		 print('UG D-WEIGHT IS 31LBS')
+	
+	 if option == 'pg' or 'PG':
+		 print('PG D-WEIGHT IS TBD')
 	 
  if input('REPEAT?(Y/N): ') == 'n':
+ 
 	 break
  
